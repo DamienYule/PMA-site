03b4f2b5d75bf3e04b486e5a05f331fc79b1b6be
DamienYule <47954095+DamienYule@users.noreply.github.com>
2020-04-06 23:48:43 -0400

    First Javascript entry

---
+++ resources/js/script.js
@@ -0,0 +1,46 @@
+//Traslation form
+function openFormTranslation() {
+document.getElementById("popupFormTranslation").style.display = "block";
+}
+function closeFormTranslation() {
+document.getElementById("popupFormTranslation").style.display = "none";
+}
+//Transcription form
+function openFormTranscription() {
+document.getElementById("popupFormTranscription").style.display = "block";
+}
+function closeFormTranscription() {
+document.getElementById("popupFormTranscription").style.display = "none";
+}
+//Interpreting form
+function openFormInterpreting() {
+document.getElementById("popupFormInterpreting").style.display = "block";
+}
+function closeFormInterpreting() {
+document.getElementById("popupFormInterpreting").style.display = "none";
+}
+//CommercialPrinting form
+function openFormCommercialPrinting() {
+document.getElementById("popupFormCommercialPrinting").style.display = "block";
+}
+function closeFormCommercialPrinting() {
+document.getElementById("popupFormCommercialPrinting").style.display = "none";
+}
+//Editing form
+function openFormEditing() {
+document.getElementById("popupFormEditing").style.display = "block";
+}
+function closeFormEditing() {
+document.getElementById("popupFormEditing").style.display = "none";
+}
+//And More form
+function openFormOther() {
+document.getElementById("popupFormOther").style.display = "block";
+}
+function closeFormOther() {
+document.getElementById("popupFormOther").style.display = "none";
+}
+
+
+
+console.log ("Hello!!")
